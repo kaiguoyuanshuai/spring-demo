2dac5b2846e797de4f62bd231b5b21e756a748e0
zhengkk <zheng_keke@126.com>
2018-09-25 21:54:59 +0800

    kafka基础使用

--- src/main/java/com/yuanshuai/kafka/KafkaProducerDemo.java
+++ src/main/java/com/yuanshuai/kafka/KafkaProducerDemo.java
@@ -2,8 +2,11 @@ package com.yuanshuai.kafka;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 /*
  * 描述:
@@ -11,9 +14,9 @@ import java.util.Properties;
  * @auth zhengkk19738
  * @time 2018/9/25
  */
-public class KafkaProducerDemo implements Runnable {
+public class KafkaProducerDemo extends Thread {
 
-    public final KafkaProducer<Integer, String> kafkaProducer;
+    public final KafkaProducer<String, String> kafkaProducer;
 
     private final String topic;
     private final boolean isAysnc;
@@ -21,27 +24,45 @@ public class KafkaProducerDemo implements Runnable {
 
     public KafkaProducerDemo(String topic, boolean isAysnc) {
         Properties properties = new Properties();
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:9092,kafka2:9092，kafka3:9092");
         properties.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaProducerDemo01");
         properties.put(ProducerConfig.ACKS_CONFIG, "-1"); //自动
-        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.IntegerSerializer");
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 
-        kafkaProducer = new KafkaProducer<Integer, String>(properties);
+        kafkaProducer = new KafkaProducer<String, String>(properties);
         this.topic = topic;
         this.isAysnc = isAysnc;
     }
 
     @Override
     public void run() {
-        int num = 0 ;
-        while (num <50 ){
+        int num = 0;
+        String messag = "message_" + num;
+        String key = "key_" + num;
+        while (num < 50) {
+            try {
+                RecordMetadata recordMetadata = kafkaProducer.send(new ProducerRecord<String,String>(topic,key, messag)).get();
+                System.out.println("sync-offset:" + recordMetadata.offset() +
+                        "->partition" + recordMetadata.partition());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+
+            num++;
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
 
         }
     }
 
-
     public static void main(String[] args) {
-
+        new KafkaProducerDemo("test", true).start();
     }
 }
